e2e94b6277b64179d79cf0f9d6556556960a8d6e
Aliny Melquiades <alinymelquiadesdesouza@gmail.com>
2024-06-05 22:34:38 -0300

    Update Principal.java

--- src/main/java/Exercicio2/Principal.java
+++ src/main/java/Exercicio2/Principal.java
@@ -1,20 +1,22 @@
 package Exercicio2;
-
+import java.lang.String;
 
     //O código (Classe Principal) exibe todos os atributos e chama todos os métodos envolvidos no processo.
 
-    public class Principal {
+    public class Principal extends Pessoa {
 
         public static void main(String[] args) {
 
-            String nome = null;
-            int idade = 0;
-            String tipo = null;
+            String nome = "Aliny";
+            int idade = 21;
 
             // Exibir todos os atributos
-            System.out.println("Nome : " + nome);
-            System.out.println("Idade: " + idade);
-            System.out.println("Tipo: " + tipo);
+            System.out.println(nome);
+            System.out.println(idade);
+
+            //Chama o Método falar
+            Pessoa pessoa = new Pessoa();
+            Principal.falar("Olá!");
 
 
         }
